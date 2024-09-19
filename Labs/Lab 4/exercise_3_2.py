b72b27facbf2acbe64183d7374f3acbf4a32e2ef
Trestan Simon <trestansimon@gmail.com>
2024-09-19 13:45:53 -0600

    Update Lab 4 `exercise_3_2.py`

--- Labs/Lab 4/exercise_3_2.py
+++ Labs/Lab 4/exercise_3_2.py
@@ -1,4 +1,6 @@
 import numpy as np
+from prelab_fixed_point import fixedpt
+from prelab_exercise_1 import order_of_convergence
 
 
 def aitken_x(x, tol, n_max):
@@ -8,3 +10,20 @@ def aitken_x(x, tol, n_max):
         if abs(p[n] - p[n-1]) < tol:
             return np.array(p)
     raise Exception("Failed")
+
+
+def g(x):
+    return np.sqrt(10. / (x + 4))
+
+
+pstar = 1.3652300134140976
+p0 = 1.5
+n_max = 100
+tol = 1e-10
+
+[p, x_all, ier] = fixedpt(g, p0, tol, n_max)
+print(f"Fixed point method took {len(x_all)} iterations.")
+aitken = aitken_x(x_all, tol, n_max)
+print(f"Aitken method took {len(aitken)} iterations.")
+order = order_of_convergence(pstar, aitken)
+print(f"Aitken's order of convergence was {order}.")
