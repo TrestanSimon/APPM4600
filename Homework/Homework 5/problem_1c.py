75ca88db7bc465b5e302c44872e4a42d9322da7a
Trestan Simon <trestansimon@gmail.com>
2024-10-01 13:14:55 -0600

    Add Homework 1 final problem 1 code

--- Homework/Homework 5/problem_1c.py
+++ Homework/Homework 5/problem_1c.py
@@ -2,10 +2,10 @@ import numpy as np
 np.set_printoptions(precision=16)
 
 
-def fixedpt_nd(F, A_inv, x0, tol, n_max):
+def newton_nd(F, J, x0, tol, n_max):
     x = [x0]
     for i in range(n_max):
-        y = A_inv @ F(x[i])
+        y = np.linalg.solve(J(x[i]), F(x[i]))
         x.append(x[i] - y)
         if np.linalg.norm(np.abs(y), np.inf) < tol:
             return np.array(x)
@@ -19,13 +19,16 @@ def F(x):
     ])
 
 
-A_inv = np.array([
-    [1/6., 1/18.],
-    [0., 1/6.]
-])
-x0 = [1, 1]
+def J(x):
+    return np.array([
+        [6. * x[0], -2. * x[1]],
+        [3. * x[1] ** 2 - 3. * x[0] ** 2, 6. * x[0] * x[1]]
+    ])
+
+
+p0 = [1, 1]
 tol = 1e-16
-fp = fixedpt_nd(F, A_inv, x0, tol, 100)
+fp = newton_nd(F, J, p0, tol, 100)
 
 x = fp[-1]
 print(f"(x, y) = ({x[0]}, {x[1]})")
